e72e259835e293ef37fa6afc4a2f2e479af3b42f
guni12 <gunvor@behovsbo.se>
2018-02-18 20:13:00 +0100

    changed default

--- behovsboboxen/html/js/script.js
+++ behovsboboxen/html/js/script.js
@@ -293,7 +293,7 @@ makeajax($acpArray);
         ARRAY.save($area.data('text'),$acpArray);
 
         $nrofrooms.data('text', $nrofrooms.html()).html('');
-        var $input = $('<select class="roomselect" name="room"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option></select>')
+        var $input = $('<select class="roomselect" name="room"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option></select>')
         .val($nrofrooms.data('text'))
         .width($nrofrooms.width()-$nrofrooms.width()+100);
         $nrofrooms.append($input); 
