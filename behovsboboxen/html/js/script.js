64f9f46314c974de6ef20bd41608f024e1df3925
guni12 <gunvor@behovsbo.se>
2018-02-19 16:06:25 +0100

    default update

--- behovsboboxen/html/js/script.js
+++ behovsboboxen/html/js/script.js
@@ -316,7 +316,7 @@ console.log("$load.data('text'): " + $load.data('text'));
         ARRAY.save($percent.data('text'),$acpArray);
 
         $percentlevel.data('text', $percentlevel.html()).html('');
-        var $input = $('<select class="levelselect" name="level"><option value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option></select>')
+        var $input = $('<select class="levelselect" name="level"><option value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option></select>')
         .val($percentlevel.data('text'))
         .width($percentlevel.width()-$percentlevel.width()+100);
         $percentlevel.append($input); 
